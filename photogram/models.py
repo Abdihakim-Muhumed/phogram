29b756c005333011f6ecc05abce1e1054cb18fb1
“Abdihakim <abdihakim.0017@gmail.com.comgit config --global user.name abdihakim17git config --global user.email abdihakim.0017@gmail.com.com>
2020-10-19 15:22:31 +0300

    First migration

--- photogram/models.py
+++ photogram/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Profile(models.Model):
+    username = models.CharField(max_length=30)
     bio = models.CharField(max_length=150)
 
     def save_category(self):
@@ -11,13 +12,13 @@ class Profile(models.Model):
         self.delete()
 
     def __str__(self):
-        return self.bio
+        return self.username
 
 class Photo(models.Model):
     name = models.CharField(max_length=30)
     caption = models.CharField(max_length=100)
-    profile = models.ForeignKey(Profile)
-    likes = models.CharField()
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    likes = models.IntegerField()
     comments = models.CharField(max_length=100)
 
     def __str__(self):
