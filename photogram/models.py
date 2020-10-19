180d1421cf5d69828be27c71779a457620f327ef
“Abdihakim <abdihakim.0017@gmail.com.comgit config --global user.name abdihakim17git config --global user.email abdihakim.0017@gmail.com.com>
2020-10-19 15:02:31 +0300

    Created model Profile

--- photogram/models.py
+++ photogram/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    bio = models.CharField(max_length=150)
+
+    def save_category(self):
+         self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    def __str__(self):
+        return self.bio
+        
